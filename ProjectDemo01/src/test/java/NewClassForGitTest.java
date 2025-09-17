832f7b84228356ef0d71bf1c90d3b5419e69334c
KAIF KHAN <KAIF KHAN@DESKTOP-N2HG8IN>
2025-09-17 13:54:03 +0530

    commit

--- ProjectDemo01/src/test/java/NewClassForGitTest.java
+++ ProjectDemo01/src/test/java/NewClassForGitTest.java
@@ -7,5 +7,6 @@ public void myTestMethod() {
 	System.out.println("this is simple print statement 02 ");
 	System.out.println("this is simple print statement 03");
 	System.out.println("this is simple print statement 04");
+	System.out.println("this is simple print statement 05");
 }
 }
